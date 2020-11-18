da13fda9e967ec0658516d92169c2fe3304a7c15
Sanne <sannelindholm@hotmail.com>
2020-11-18 21:10:11 +0100

    Correcting typo in InputFields.jsx

--- src/components/InputFields.jsx
+++ src/components/InputFields.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
 
-const InputFields = ({ inChangeHandler }) => {
+const InputFields = ({ onChangeHandler }) => {
   return (
     <>
       <label>Distance</label>
       <input
-      onChange={this.onChangeHandler}
+      onChange={onChangeHandler}
       name="distance"
       id="distance"
       ></input>
 
-      <select onChange={this.onChangeHandler} name="gender" id="gender">
+      <select onChange={onChangeHandler} name="gender" id="gender">
         <option value="female">Female</option>
         <option value="male">Male</option>
       </select>
 
       <label>Age</label>
-      <input onChange={this.onChangeHandler} name="age" id="age"></input>
+      <input onChange={onChangeHandler} name="age" id="age"></input>
     </>
   )
 };
