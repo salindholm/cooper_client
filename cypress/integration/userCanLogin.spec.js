aa1bb2145a7366d032a09a0606014485f0c87d4b
William <willspecker@gmail.com>
2020-11-19 11:17:41 +0100

    creating mock login credentials in json file changing login test to use mock credentials

--- cypress/integration/userCanLogin.spec.js
+++ cypress/integration/userCanLogin.spec.js
@@ -1,24 +1,42 @@
 /// <reference types="Cypress" />
 
-describe("User authenticates", () => {
+describe("User can log in", () => {
   beforeEach(() => {
+    cy.server();
     cy.visit("/");
   });
 
-  it("successfully with credentials", () => {
+  it("successfully", () => {
+    cy.route({
+      method: "POST",
+      url: "http://localhost:3000/api/v1/auth/sign_in",
+      response: "fixture:login.json",
+      headers: {
+        uid: "user@mail.com"
+      }
+    });
     cy.get("#login").click();
     cy.get("#login-form").within(() => {
-      cy.get("#email").type("exampleX@example.com");
+      cy.get("#email").type("user@mail.com");
       cy.get("#password").type("password");
       cy.get('button').contains('Submit').click()
     }); 
     cy.get("p#login-success").should("contain", "Hi", "#email");
   });
 
-  it("unsuccessfully with invalid credentials", () => {
+  it("with invalid credentials", () => {
+    cy.route({
+      method: "POST",
+      url: "http://localhost:3000/api/v1/auth/sign_in",
+      status: "401",
+      response: {
+        errors: ["Invalid login credentials. Please try again."],
+        success: false
+      }
+    });
     cy.get("#login").click();
     cy.get("#login-form").within(() => {
-      cy.get("#email").type("example@mail.com");
+      cy.get("#email").type("user@mail.com");
       cy.get("#password").type("wrongpassword");
       cy.get('button').contains('Submit').click()
     });
