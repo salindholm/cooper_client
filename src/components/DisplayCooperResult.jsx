d6947924e9c2f756cfd8f9669b4f533de1805748
Sanne <sannelindholm@hotmail.com>
2020-11-19 11:41:57 +0100

    Adding conditional in render method to show the save-result button in DisplayCooperResult.jsx.

--- src/components/DisplayCooperResult.jsx
+++ src/components/DisplayCooperResult.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import cooperCalculator from "../modules/cooperCalculator";
+import saveData from "../modules/performanceData";
 
-const DisplayCooperResult = ({ distance, gender, age }) => {
-  const calculate = () => {
-    return cooperCalculator(distance, gender, age);
-  };
+const DisplayCooperResult = ({
+  distance,
+  gender,
+  age,
+  authenticated,
+  entrySaved,
+  entryHandler
+ }) => {
+  const result = cooperCalculator(distance, gender, age);
 
   const propsPassed = distance && age ? true : false;
 
@@ -16,10 +22,20 @@ const DisplayCooperResult = ({ distance, gender, age }) => {
             {age} year old {gender} running {distance} meters.
           </p>
           <p id="cooper-result">Result: {calculate()}</p>
+          {authenticated && !entrySaved ? (
+            <button
+            id="save-result"
+            onClick={() => saveData(result, entryHandler)}
+            >
+              Save Entry
+            </button>
+          ) : (
+            <p id="response-message">Your entry was saved</p>
+          )}
         </>
       )}
     </>
-  )
-}
+  );
+};
 
 export default DisplayCooperResult;
