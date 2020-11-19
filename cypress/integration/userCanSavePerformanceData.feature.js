c1443a98955f4270e72924dd1af72c9f80a3df6c
Sanne <sannelindholm@hotmail.com>
2020-11-19 12:37:03 +0100

    Changing route url to performace_data for POST action in test and adding saved entry to onchangehandler in app.jsx.

--- cypress/integration/userCanSavePerformanceData.feature.js
+++ cypress/integration/userCanSavePerformanceData.feature.js
@@ -8,22 +8,22 @@ describe("User attempts to save data", () => {
       url: "http://localhost:3000/api/v1/auth/sign_in",
       response: "fixture:login.json",
       headers: {
-        uid: "user@mail.com"
+        uid: "exampleX@example.com"
       }
     });
     cy.route({
       method: "POST",
-      url: "http://localhost:3000/api/v1/auth/sign_in",
+      url: "http://localhost:3000/api/v1/auth/performance_data",
       response: {},
       headers: {
-        uid: "user@mail.com"
+        uid: "exampleX@example.com"
       }
     });
     cy.visit("/");
 
     cy.get("#login").click();
     cy.get("#login-form").within(() => {
-      cy.get("#email").type("user@mail.com");
+      cy.get("#email").type("exampleX@example.com");
       cy.get("#password").type("password");
       cy.get('button').contains('Submit').click()
     }); 
