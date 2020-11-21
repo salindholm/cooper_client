ac05bb43680351ebab0708f92caa9f3feb807c45
William <willspecker@gmail.com>
2020-11-21 13:44:10 +0100

    adding formatting to select menu

--- src/components/InputFields.jsx
+++ src/components/InputFields.jsx
@@ -1,24 +1,23 @@
 import React from 'react'
-import { Form, Divider, Segment, Icon, Header } from 'semantic-ui-react'
+import { Form, Divider, Segment, Icon, Header, Grid } from 'semantic-ui-react'
+import "../styling/inputFields.css"
 
 const InputFields = ({ onChangeHandler }) => {
   return (
     <Segment basic textAlign='center'>
       <Header as='h3'>Check Your Results <Icon name='heartbeat'/> </Header>
       <Form>
-        <Form.Field>
-          <label>Age</label>
+        <Form.Group >
+          <label id="age-label">Age:</label>
           <input onChange={onChangeHandler} name="age" id="age"></input>
-        </Form.Field>
-        <Form.Field>
-          <label>Distance</label>
+          <label id="distance-label">Distance:</label>
           <input
           onChange={onChangeHandler}
           name="distance"
           id="distance"
           > 
           </input>
-        </Form.Field>
+        </Form.Group>
         <select onChange={onChangeHandler} name="gender" id="gender">
           <option value="female">Female</option>
           <option value="male">Male</option>
