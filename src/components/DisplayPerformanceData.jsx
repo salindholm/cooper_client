f5a07e44591876898289d0188e3b502f59f1141c
William <willspecker@gmail.com>
2020-11-22 14:02:20 +0100

    Fixing typos in DisplayPerformanceData.jsx

--- src/components/DisplayPerformanceData.jsx
+++ src/components/DisplayPerformanceData.jsx
@@ -49,6 +49,7 @@ class DisplayPerformanceData extends Component {
           'rgba(54, 162, 235, 0.2)',
           'rgba(255, 206, 86, 0.2)',
           'rgba(75, 192, 192, 0.2)'
+        ],
       }],
     };
 
